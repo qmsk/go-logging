19f0cd34a9e0bdfd5ea4d3b5b5f6aeca2ad0b725
Tero Marttila <terom@fixme.fi>
2018-02-24 18:49:00 +0200

    split out util/logging package

---
+++ logging.go
@@ -0,0 +1,32 @@
+package logging
+
+type Logging struct {
+	Debug Logger
+	Info  Logger
+	Warn  Logger
+	Error Logger
+}
+
+func (log *Logging) Debugf(format string, args ...interface{}) {
+	if log != nil && log.Debug != nil {
+		log.Debug.Printf(format, args...)
+	}
+}
+
+func (log *Logging) Infof(format string, args ...interface{}) {
+	if log != nil && log.Info != nil {
+		log.Info.Printf(format, args...)
+	}
+}
+
+func (log *Logging) Warnf(format string, args ...interface{}) {
+	if log != nil && log.Warn != nil {
+		log.Warn.Printf(format, args...)
+	}
+}
+
+func (log *Logging) Errorf(format string, args ...interface{}) {
+	if log != nil && log.Error != nil {
+		log.Error.Printf(format, args...)
+	}
+}
